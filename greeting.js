928e89b58a9f687b492de27d27a1eece9795de6b
chojun917 <96730816+chojun917@users.noreply.github.com>
2022-01-02 08:10:54 +0900

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,25 @@
+const loginForm = document.querySelector("#loginform");
+const loginInput = loginform.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+function onLoginBtnSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem("USERNAME_KEY", username);
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+if(savedUsername === null){
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit",onLoginBtnSubmit);
+} else {
+   greeting.innerText = "Hello " + savedUsername;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+   
+}
